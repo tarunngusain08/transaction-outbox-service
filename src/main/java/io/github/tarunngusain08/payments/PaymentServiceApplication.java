448ffdb36a84a10962aebe725455746ad1d4d633
tarunngusain08 <prudent.tarun0808@gmail.com>
2026-08-08 08:49:24 +0530

    chore: initialize Spring Boot payment service

---
+++ src/main/java/io/github/tarunngusain08/payments/PaymentServiceApplication.java
@@ -0,0 +1,14 @@
+package io.github.tarunngusain08.payments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication
+public class PaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentServiceApplication.class, args);
+    }
+}
---
+++ src/test/java/io/github/tarunngusain08/payments/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.tarunngusain08.payments;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PaymentServiceApplicationTests {
+
+    @Test
+    void applicationClassIsAvailable() {
+        assertThat(PaymentServiceApplication.class).isNotNull();
+    }
+}
